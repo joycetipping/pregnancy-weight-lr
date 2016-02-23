b667e50a7ef0e2776e7374a834a1c7051f819438
Joyce Tipping <joycetipping@gmail.com>
2016-02-22 20:10:57 -0700

    Perl script for parsing the times.

---
+++ parse.pl
@@ -0,0 +1,12 @@
+use strict; use warnings;
+use Date::Parse;
+
+open(my $fh, '<', 'weights.csv') || die 'cannot open';
+my $weight_list = "day,weight\n";
+while (my $line = <$fh>) {
+  chomp $line;
+  my ($date, $weight) = split(/,/, $line);
+  $weight_list .= str2time($date) . ",$weight\n";
+}
+open(my $nf, '>', 'data.csv') || die 'cannot open';
+print $nf $weight_list;
